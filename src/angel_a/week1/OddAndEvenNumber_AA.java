308efb7ff523566c5bac107fa0d9eafd9f99f954
Angel75 <angelov.pro@gmail.com>
2025-01-10 11:09:32 -0600

    Trying to see

--- src/angel_a/week1/OddAndEvenNumber_AA.java
+++ src/angel_a/week1/OddAndEvenNumber_AA.java
@@ -4,5 +4,7 @@ public class OddAndEvenNumber_AA {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        System.out.println("Trying to see if it works");
     }
 }
