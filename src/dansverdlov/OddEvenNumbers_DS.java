6c7a559413500fcda41e0faa75e336a2470de619
dbs92590 <dbs92590@gmail.com>
2025-01-10 19:39:28 -0600

    First Task Finished

--- src/dansverdlov/OddEvenNumbers_DS.java
+++ src/dansverdlov/OddEvenNumbers_DS.java
@@ -1,7 +1,19 @@
 package dansverdlov;
 
 public class OddEvenNumbers_DS {
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        System.out.println(oddOrEven(5));
+
+    }
+
+
+    public static String oddOrEven(int num1) {
+        if (num1 % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
     }
 }
