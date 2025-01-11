e154b41eacde8bc9141c0ff3887fd6ecc175afa5
Beste <fatmaustun1@gmail.com>
2025-01-10 20:01:09 -0600

    Odd and even project is done.

---
+++ src/fatmaustun/OddAndEvenFU.java
@@ -0,0 +1,20 @@
+package fatmaustun;
+
+public class OddAndEvenFU {
+    public static void main(String[] args) {
+
+        int number = 7;
+        int number2 =5;
+        String result = oddEven(number);
+        System.out.println(result);
+
+    }
+
+    public static String oddEven(int number) {
+        if (number % 2 == 0) {
+            return number + " is even.";
+        } else {
+            return number + " is odd.";
+        }
+    }
+}
