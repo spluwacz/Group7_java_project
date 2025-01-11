9f8a8fbd25f9cf4a96a4c47e48bd9ef94fdfb663
kryst <spluwacz@aol.com>
2025-01-10 19:19:44 -0600

    Added week1 folder

--- src/krystian/week1/OddAndEven_K_T.java
+++ src/krystian/week1/OddAndEven_K_T.java
@@ -1,9 +1,13 @@
-package krystian;
+package krystian.week1;
 
 public class OddAndEven_K_T {
 
     public static void main(String[] args) {
         System.out.println("Hello Word");
+
+
+
+
     }
 
 }
