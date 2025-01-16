6d8f585f203c437d6624fc0bb5643b0e0a5adf6f
kryst <spluwacz@aol.com>
2025-01-16 15:09:32 -0600

    finished tasks and added notes

--- src/krystian/week1/OddAndEven_K_T.java
+++ src/krystian/week1/OddAndEven_K_T.java
@@ -6,24 +6,26 @@ public class OddAndEven_K_T {
 
     public static void main(String[] args) {
         // did this as an extra so different numbers can be entered into console, instead of calling method
-        System.out.println("enter num: ");
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
+        //System.out.println("enter num: ");
+        //Scanner scanner = new Scanner(System.in);
+       // int num = scanner.nextInt();
 
-        System.out.println(oddOrEven(num));
-        scanner.close();
+       // System.out.println(oddOrEven(num));
+        //scanner.close();
 
         //calling in main method
-        System.out.println(oddOrEven(2));
+        oddOrEven(30);
+
     }
 
-                                                //created public access so i can access from anywhere, static
-                                                // modifier because i want to access without creating an object
-    public static String oddOrEven(int num1) {  // method takes int argument with variable name num1 as we num for scanner
-        if (num1 % 2 == 0) {     //im going to check if a number can be divided by 2 with a remainder of 0
-            return "Even";         // if it is it will return even
-        } else {                  // if not only option is to return odd
-            return "Odd";
+                                                                 //created public access so i can access from anywhere, static
+                                                                 // modifier because i want to access without creating an object
+    public static void oddOrEven(int num) {                             // method takes int argument with variable name num1 as we num for scanner
+        if (num % 2 == 0) {
+                                                                             //im going to check if a number can be divided by 2 with a remainder of 0
+            System.out.println("your number " + num + " Even");         // if it is it will return even
+        } else {                                                     // if not only option is to return odd
+            System.out.println("your number " + num + " Odd");
         }
 
     }
