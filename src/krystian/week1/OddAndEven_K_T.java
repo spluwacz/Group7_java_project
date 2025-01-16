88f5376ad0d3284af7b4707428c36e0167c9c71c
kryst <spluwacz@aol.com>
2025-01-16 12:26:10 -0600

    finished taks and added notes

--- src/krystian/week1/OddAndEven_K_T.java
+++ src/krystian/week1/OddAndEven_K_T.java
@@ -1,13 +1,32 @@
 package krystian.week1;
 
+import java.util.Scanner;
+
 public class OddAndEven_K_T {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        // did this as an extra so different numbers can be entered into console, instead of calling method
+        System.out.println("enter num: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
 
+        System.out.println(oddOrEven(num));
+        scanner.close();
 
+        //calling in main method
+        System.out.println(oddOrEven(2));
+    }
 
+                                                //created public access so i can access from anywhere, static
+                                                // modifier because i want to access without creating an object
+    public static String oddOrEven(int num1) {  // method takes int argument with variable name num1 as we num for scanner
+        if (num1 % 2 == 0) {     //im going to check if a number can be divided by 2 with a remainder of 0
+            return "Even";         // if it is it will return even
+        } else {                  // if not only option is to return odd
+            return "Odd";
+        }
 
     }
+
 
 }
