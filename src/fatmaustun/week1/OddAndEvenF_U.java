ca920ed25d531365362d55ced8f6e0db6799c680
Beste <fatmaustun1@gmail.com>
2025-01-19 14:17:17 -0600

    Second task is done...

--- src/fatmaustun/week1/OddAndEvenF_U.java
+++ src/fatmaustun/week1/OddAndEvenF_U.java
@@ -14,20 +14,4 @@ public class OddAndEvenF_U {
             return number + " is odd.";
         }
     }
-
-    public static class OddAndEvenF_U {
-        public static void main(String[] args) {
-            int number = 7;
-            String result = oddEven(number);
-            System.out.println(result);
-        }
-
-        public static String oddEven(int number) {
-            if (number % 2 == 0) {
-                return number + " is even.";
-            } else {
-                return number + " is odd.";
-            }
-        }
-    }
 }
