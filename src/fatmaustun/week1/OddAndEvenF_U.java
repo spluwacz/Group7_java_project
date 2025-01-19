5a87844465a6891c19bd2ea5f0c9351e1879ce79
Beste <fatmaustun1@gmail.com>
2025-01-19 09:24:08 -0600

    Second task is done.

---
+++ src/fatmaustun/week1/OddAndEvenF_U.java
@@ -0,0 +1,33 @@
+package fatmaustun.week1;
+
+public class OddAndEvenF_U {
+    public static void main(String[] args) {
+        int number = 11;
+        String result = oddEven(number);
+        System.out.println(result);
+    }
+
+    public static String oddEven(int number) {
+        if (number % 2 == 0) {
+            return number + " is even.";
+        } else {
+            return number + " is odd.";
+        }
+    }
+
+    public static class OddAndEvenF_U {
+        public static void main(String[] args) {
+            int number = 7;
+            String result = oddEven(number);
+            System.out.println(result);
+        }
+
+        public static String oddEven(int number) {
+            if (number % 2 == 0) {
+                return number + " is even.";
+            } else {
+                return number + " is odd.";
+            }
+        }
+    }
+}
