a3eff00942cd2e979ca7f32daec4b912d9b13772
amies <amieshopping33@gmail.com>
2025-01-20 14:37:52 -0600

    Package created first task done

---
+++ src/amieluk/Week2/FinRa.java
@@ -0,0 +1,20 @@
+package amieluk.Week2;
+
+public class FinRa {
+    public static void main(String[] args) {
+
+        int num=30;
+
+        for(int i = 1; i <= 30 ; i++){
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
