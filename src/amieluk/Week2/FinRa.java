259796835a47e6eccb1d645649046a285be03501
amies <amieshopping33@gmail.com>
2025-01-20 15:01:35 -0600

    Package created second week done

--- src/amieluk/Week2/FinRa.java
+++ src/amieluk/Week2/FinRa.java
@@ -3,18 +3,25 @@ package amieluk.Week2;
 public class FinRa {
     public static void main(String[] args) {
 
-        int num=30;
+      //Write a function that prints out the numbers from 1 to 30 but for numbers that are
+        //        a multiple of 3, print "FIN" instead of the number, and for numbers that are a multiple
+        //            of 5, print "RA" instead of the number. For numbers that are a multiple of both 3 and 5,
+        //            print "FINRA" instead of the number.
 
-        for(int i = 1; i <= 30 ; i++){
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FINRA");
-            } else if (i % 5 == 0) {
-                System.out.println("RA");
-            } else if (i % 3 == 0) {
-                System.out.println("FIN");
-            } else {
-                System.out.println(i);
+
+            String result = "";
+
+            for(int i=1; i <= 30; i++) {
+                if(i % 5==0 && i %3 ==0)
+                    result += "FINRA ";
+                else if(i%5 == 0)
+                    result += "RA ";
+                else if(i%3==0)
+                    result+="FIN ";
+                else
+                    result += i+" ";
             }
+            System.out.println(result);
         }
     }
-}
+
