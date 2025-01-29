37147872e86de021922e8ca3311599fbe0f4db81
amies <amieshopping33@gmail.com>
2025-01-29 14:47:01 -0600

    week 3 task done

---
+++ src/amieluk/Week3/NumberSwap_AL.java
@@ -0,0 +1,19 @@
+package amieluk.Week3;
+
+public class NumberSwap_AL {
+    public static void main(String[] args) {
+
+        // Swap two numbers without using a third variable
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println("Before Swapping :: x=" + x + ", y=" + y);
+
+        //logic for swapping
+        x= x+y; //10+20 = 30
+        y = x-y;//30-20=10
+        x = x-y; //30-10=20
+        System.out.println("After Swapping :: x=" + x + ", y=" + y);
+    }
+}
