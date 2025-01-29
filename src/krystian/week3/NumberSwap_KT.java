4c4372159b9dde3b4530690a5ed99f7cdb10c932
kryst <spluwacz@aol.com>
2025-01-28 20:37:05 -0600

    finished 2nd task

--- src/krystian/week3/NumberSwap_KT.java
+++ src/krystian/week3/NumberSwap_KT.java
@@ -2,10 +2,41 @@ package krystian.week3;
 
 public class NumberSwap_KT {
 
+    public static void main(String[] args) {
+
+/*
+        int a = 5;
+        int b = 10;
+
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+ */
+        int a = 5;
+        int b = 10;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("--------------------");
+        a = a + b; //15
+        b = a - b; //5
+        a = a - b; //10
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
 
 
 
 
+
+
+    }
 
 
 }
