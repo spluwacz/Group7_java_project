fa05709cca5a568e5747e0b95b96090507b5d8e6
KSaltanat-coder <saltakaldybaeva@gmail.com>
2025-01-30 11:32:40 -0600

    Project Swap variables done

--- src/saltanat/week1/OddAndEvenNumber_ST.java
+++ src/saltanat/week1/OddAndEvenNumber_ST.java
@@ -2,6 +2,29 @@ package saltanat.week1;
 
 public class OddAndEvenNumber_ST {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        for (int i = 0; i <= 10; i+=2) {
+            sumEven += i;
+        }
+        for (int i = 1; i <= 10; i+=2) {
+            sumOdd += i;
+        }
+        /*
+        for (int i = 0; i <= 10; i++) {
+            if (i%2==0){
+            sumEven += i;
+            }else{
+            sumOdd +=i;
+         */
+        System.out.println("Sum of even: " + sumEven);
+        System.out.println("Sum of odd: " + sumOdd);
+
+
+
+
+
     }
 }
+
